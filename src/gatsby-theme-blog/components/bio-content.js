4ab1cb1028858e58a259d2f992dcb37a25b83ed5
disturbing <joseph@kintohub.com>
2021-01-11 18:41:16 +0800

    :bug: bug(Bio): fix bio position

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -5,8 +5,10 @@ export default function Bio() {
   return (
     <>
       <Styled.a href="https://codingcoop.io/">Joseph Cooper</Styled.a>
-      <br></br>
-      Live, Love and Code
+      <br/>
+      Born in 1990, with 20+ games built, 11 games/apps/products shipped, 7 startups made and 20 years of coding.
+      <br/><br/>
+      This is a personal blog, mostly for myself, but open to the world. It includes reflections of the past and notes for the future with hopes to increase awareness of how the invaluable currency of time is spent well, traded fairly and wasted often.
     </>
   )
 }
