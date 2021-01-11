2ee8d7dec18db2d4d37f3176da9aa51890023c87
disturbing <joseph@kintohub.com>
2021-01-11 15:49:30 +0800

    :construction: feat(Blog): hello world

---
+++ my-themed-blog/src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Styled } from "theme-ui"
+
+export default function Bio() {
+  return (
+    <>
+      <Styled.a href="https://codingcoop.io/">Joseph Cooper</Styled.a>
+      <br></br>
+      Live, Love and Code
+    </>
+  )
+}
