b5ab584b0bfed06c4a5a5f12db75a44c1ad56ffe
disturbing <joseph@kintohub.com>
2021-01-11 18:27:08 +0800

    :alien: feat(Bio): bio updated and linkedin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,18 +7,22 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `Dance Lessons in Software, Product, Management, Entreprenuership and People`,
+    title: `Thoughts on Software Development, Product, Data, Leadership, Entreprenuership and being Human`,
     author: `Joseph Cooper`,
-    description: `A personal blog, mostly for myself, but open to the world. Mostly reflections of the past and notes for the future in effort to increase awareness of how invaluable time is spent well, traded fairly and wasted often.`,
+    description: `A personal blog, mostly for myself, but open to the world. Mostly reflections of the past and notes for the future in effort to increase awareness of how invaluable time is spent well, traded fairly and wasted often. I'm 30 years old with 20+ games built, 5+ products shipped, 7 startups made and 20 years of coding.`,
     siteUrl: "https://codingcoop.io/",
     social: [
       {
-        name: `twitter`,
+        name: `Twitter`,
         url: `https://twitter.com/codingcoop/`,
       },
       {
-        name: `github`,
+        name: `GitHub`,
         url: `https://github.com/disturbing/`,
+      },
+      {
+        name: `LinkedIn`,
+        url: `https://www.linkedin.com/in/codingcoop`,
       },
     ],
   },
