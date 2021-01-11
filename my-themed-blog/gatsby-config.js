b0115d3d2cc3f017583c72e218e50d745038e302
disturbing <joseph@kintohub.com>
2021-01-11 15:49:30 +0800

    :construction: feat(Blog): hello world

---
+++ my-themed-blog/gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Dance Lessons in Software, Product, Management, Entreprenuership and People`,
+    author: `Joseph Cooper`,
+    description: `A personal blog, mostly for myself, but open to the world. Mostly reflections of the past and notes for the future in effort to increase awareness of how invaluable time is spent well, traded fairly and wasted often.`,
+    siteUrl: "https://codingcoop.io/",
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/codingcoop/`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/disturbing/`,
+      },
+    ],
+  },
+}
